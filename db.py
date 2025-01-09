7953646e353fc2293b2897f2ea241baf2302ebce
rogerpajaro58 <rogerpajaro58@gmail.com>
2025-01-08 22:45:49 -0500

    primer despliegue

--- db.py
+++ db.py
@@ -1,18 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import OperationalError
 
 DATABASE_URI = "postgresql://agendadb:iftpt8WFitmuUjXTjRy534bn4wPueGwJ@dpg-ctust3lds78s738k7vig-a.oregon-postgres.render.com/agenda"
-
 
 def conectar():
     engine = create_engine(DATABASE_URI)
     Session = sessionmaker(bind=engine)
-    s = Session()
-    
-    if s != None:
-        print("Conexion a base de datos OK")
-    else:
-        print("Error en conexion a base de datos")   
-        
-        
-    return s
+
+    try:
+        # Intentar establecer una conexión realizando una operación simple
+        session = Session()
+        session.execute('SELECT 1')  # Simple query to check connection
+        print("Conexión a base de datos OK")
+        return session
+    except OperationalError as e:
+        print(f"Error en conexión a base de datos: {e}")
+        return None
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS  # Importa CORS
 
 from routes.userRoutes import user_api
 from routes.contactRoutes import contact_api
 
-
 app = Flask(__name__)
+
+
+CORS(app) 
 
 
 app.register_blueprint(user_api)
 app.register_blueprint(contact_api)
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
